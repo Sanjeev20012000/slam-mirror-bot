1b966249db811ac19c12a312997a4471a4ef7422
Sanjeev K <69083272+Sanjeev20012000@users.noreply.github.com>
2021-07-26 09:52:28 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -25,5 +25,6 @@ class _BotCommands:
         self.TarWatchCommand = 'tarwatch'
         self.DeleteCommand = 'del'
         self.UsageCommand = 'usage'
+        self.GplinkCommand = 'gplink'
 
 BotCommands = _BotCommands()
